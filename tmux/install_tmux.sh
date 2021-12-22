687b71a7fbdfd5fc6df8888a8c6b0d9e972a761e
kia280 <kia@csie.io>
2021-12-22 17:32:02 +0800

    [tmux] use OSC-52 escape copy

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -4,5 +4,6 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp .tmux.conf ~
+cp yank /usr/local/bin/
 
 echo "Press prefix + I (capital i, as in Install) to fetch the plugin."
